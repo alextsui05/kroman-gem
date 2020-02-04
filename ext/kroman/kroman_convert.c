3598cb81e1a53ea16808fd8705fe060b6669ac41
Alex Tsui <alextsui@pm.me>
2020-02-04 23:06:17 +0000

    :beetle: fixing error in extension build

--- ext/kroman/kroman_convert.c
+++ ext/kroman/kroman_convert.c
@@ -1,4 +1,5 @@
 #include "kroman_convert.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define KROMAN_GA 0xac00
--- lib/kroman/version.rb
+++ lib/kroman/version.rb
@@ -1,3 +1,3 @@
 module Kroman
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
